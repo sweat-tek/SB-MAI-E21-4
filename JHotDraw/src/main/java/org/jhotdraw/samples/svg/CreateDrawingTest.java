865aa1f790d7b5b0ea5db6008ed962c57de81a0b
Ásbjörn Skarphéðinsson <usska18@student.sdu.dk>
2021-12-05 12:02:15 +0100

    Add first unit test

---
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/CreateDrawingTest.java
@@ -0,0 +1,31 @@
+package org.jhotdraw.samples.svg;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.jhotdraw.draw.Drawing;
+import org.jhotdraw.draw.QuadTreeDrawing;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+/**
+ *
+ * @author asbjo
+ */
+public class CreateDrawingTest {
+    @Test
+    public void testCreateDrawing() throws Exception {
+        
+// Assert that method returns Drawing object
+        SVGView view = new SVGView();
+        Drawing drawing = new QuadTreeDrawing();
+        assertEquals(drawing, view.createDrawing());
+        
+        // Assert that method has LinkedList<InputFormat>
+        
+        // Assert that the method loads InputFormats
+        
+        
+    }
+}
